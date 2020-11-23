19c67fd9bb0c8d7295e4446ed34b1ab965760c9a
W <as5818956@gmail.com>
2020-11-23 22:23:41 +0900

    Change　Color

--- dx12pbr/src/Common/d3dApp.h
+++ dx12pbr/src/Common/d3dApp.h
@@ -130,8 +130,8 @@ protected:
 	D3D_DRIVER_TYPE md3dDriverType = D3D_DRIVER_TYPE_HARDWARE;
     DXGI_FORMAT mBackBufferFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
     DXGI_FORMAT mDepthStencilFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
-	int mClientWidth = 800;
-	int mClientHeight = 600;
+	int mClientWidth = 1600;
+	int mClientHeight = 900;
 
     //Imgui
     Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> mSrvHeap;
